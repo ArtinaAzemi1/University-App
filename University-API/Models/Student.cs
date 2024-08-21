0480383bbfd2e85d98fd79d2cdf052f17554b651
ArtinaAzemi1 <150926679+ArtinaAzemi1@users.noreply.github.com>
2024-08-21 13:25:07 +0200

    Perfundimi i modelit te Studentit

--- University-API/Models/Student.cs
+++ University-API/Models/Student.cs
@@ -1,6 +1,25 @@
-﻿namespace University_API.Models
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace University_API.Models
 {
     public class Student
     {
+        public int StudentId { get; set; }
+
+        public string? Name { get; set; }
+
+        public string? Surname { get; set; }
+
+        public DateOnly? BirthDate { get; set; }
+
+        public string? Gender { get; set; }
+
+        public string? City { get; set; }
+
+        public string? Email { get; set; }
+
+        public string? Photo { get; set; } = "studentPhoto.png";
+
+
     }
 }
