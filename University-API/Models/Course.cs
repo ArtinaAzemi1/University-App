a3e8cffb9dec23f302d7838c20c9651e4ed8bd8e
ArtinaAzemi1 <150926679+ArtinaAzemi1@users.noreply.github.com>
2024-08-22 17:49:15 +0200

    Krijimi i klases Course

---
+++ University-API/Models/Course.cs
@@ -0,0 +1,14 @@
+﻿namespace University_API.Models
+{
+    public class Course
+    {
+        public int CourseId { get; set; }
+
+        public string? Name { get; set; }
+
+        public string? Semester { get; set; }
+
+        public int? ECTS { get; set; }
+
+    }
+}
