72989cde1eca61bbfb01437540d4a077ce55e225
ArtinaAzemi1 <150926679+ArtinaAzemi1@users.noreply.github.com>
2024-08-22 17:47:33 +0200

    Shtimi i klasave Profesor dhe Departament

---
+++ University-API/Models/Department.cs
@@ -0,0 +1,14 @@
+﻿namespace University_API.Models
+{
+    public class Department
+    {
+        public int DepartmentId { get; set; }
+
+        public string? DepartmentName { get; set; }
+
+        public string? DeanName { get; set; }
+
+        public int? StafCount { get; set; }
+
+    }
+}
---
+++ University-API/Models/Professor.cs
@@ -0,0 +1,22 @@
+﻿namespace University_API.Models
+{
+    public class Professor
+    {
+        public int ProfessorId { get; set; }
+
+        public string? Name { get; set; }
+
+        public string? Surname { get; set; }
+
+        public string? BirthDate { get; set; }
+
+        public string? Gender { get; set; }
+
+        public string? City { get; set; }
+
+        public string? Email { get; set; }
+
+        public string? Photo { get; set; } = "professorPhoto.png";
+
+    }
+}
